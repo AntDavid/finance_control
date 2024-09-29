a16a72fa28e708f1f9e6e6b3f673600d76f99532
AntDavid <davidantoniodavi@gmail.com>
2024-09-29 03:59:31 -0300

    Create finance_control database and define tables for users, transactions, financial goals, goal contributions, salary, and expenses

---
+++ sql/dabase.sql
@@ -0,0 +1,71 @@
+
+-- Criação do Banco de Dado finance_control --
+CREATE DATABASE finance_control;
+
+USE finance_control;
+
+
+-- Tabela para armazenar os usuários cadastrados--
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+-- Tabela para armazenar as transações financeiras--
+CREATE TABLE transactions (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    date DATE NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    amount DECIMAL(10, 2) NOT NULL,
+    category VARCHAR(100) NOT NULL,
+    type ENUM('income', 'expense') NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
+
+-- Tabela para armazenar as metas financeiras--
+CREATE TABLE financial_goals (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    goal_name VARCHAR(255) NOT NULL,
+    target_amount DECIMAL(10, 2) NOT NULL,
+    current_amount DECIMAL(10, 2) DEFAULT 0.00,
+    due_date DATE NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+
+
+
+-- Tabela para armazenar o contribuição para as metas--
+CREATE TABLE goal_contributions (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    goal_id INT NOT NULL,
+    contribution_amount DECIMAL(10, 2) NOT NULL,
+    contribution_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (goal_id) REFERENCES financial_goals(id) ON DELETE CASCADE
+);
+
+
+
+-- Tabela para armazenar o salário--
+CREATE TABLE salary (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    amount DECIMAL(10, 2) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+
+-- Tabela para armazenar as despesas--
+CREATE TABLE expenses (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    amount DECIMAL(10, 2) NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
