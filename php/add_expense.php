67ee81426fcf9957107fb9229aaeab4d1d44d4e3
AntDavid <davidantoniodavi@gmail.com>
2024-10-01 13:33:38 -0300

    Add Expense Insertion Logic with User-Specific Association

--- php/add_expense.php
+++ php/add_expense.php
@@ -1,23 +1,32 @@
 <?php
 include_once 'conn.php';
+include_once 'check_session.php'; 
+
+$user_id = $_SESSION['user_id']; 
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $expense = $_POST['expense'];
     $description = $_POST['description'];
 
-    $conn = (new Conn())->getConnection();
-    $stmt = $conn->prepare("INSERT INTO expenses (amount, description) VALUES (?, ?)");
-    $stmt->bind_param("ds", $expense, $description);
+    if (!empty($expense) && !empty($description)) {
+        $conn = (new Conn())->getConnection();
+        
+        $stmt = $conn->prepare("INSERT INTO expenses (user_id, amount, description) VALUES (?, ?, ?)");
+        $stmt->bind_param("ids", $user_id, $expense, $description); 
 
-    if ($stmt->execute()) {
-        echo "Despesa adicionada com sucesso.";
+        if ($stmt->execute()) {
+            echo "Despesa adicionada com sucesso.";
+        } else {
+            echo "Erro ao adicionar despesa: " . $stmt->error; 
+        }
+
+        $stmt->close();
+        $conn->close();
+
+        header("Location: ../pages/budget_management.php"); 
+        exit; 
     } else {
-        echo "Erro ao adicionar despesa: " . $stmt->error;
+        echo "Por favor, preencha todos os campos."; 
     }
-
-    $stmt->close();
-    $conn->close();
-
-    header("Location: ../pages/budget_management.php"); 
 }
 ?>
